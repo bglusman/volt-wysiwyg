9efe206f2ffb999fcac35df099496a55d5e008cc
Brian Glusman <brian@glusman.me>
2015-04-23 16:20:06 -0400

    Working text attribute, debugging quill error

--- app/wysiwyg/controllers/main_controller.rb
+++ app/wysiwyg/controllers/main_controller.rb
@@ -1,10 +1,17 @@
 module Wysiwyg
   class MainController < Volt::ModelController
-    def initialize
-      Document.ready? do 
-        `var quill = new Quill('#editor');`
-        `quill.addModule('toolbar', { container: '#toolbar' });`
+
+    def initialize(attributes)
+      Document.ready? do
+        if @quill.nil?
+          puts 'think @quill is nill'
+          `var quill = new Quill('.quill-editor');`
+          @quill = `quill`
+        end
+        `#{@quill}.addModule('toolbar', { container: '.quill-toolbar' });`
+        Element.find('.quill-editor > div').html(attributes.locals[:text])
       end
     end
+
   end
 end
