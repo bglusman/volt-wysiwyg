828114d34946bb6743067256f9a6cae0467b589c
Robert H Grayson II <bobbygrayson@gmail.com>
2015-04-23 15:09:38 -0400

    proof of concept working wysiwyg tag

--- app/wysiwyg/controllers/main_controller.rb
+++ app/wysiwyg/controllers/main_controller.rb
@@ -1,4 +1,10 @@
 module Wysiwyg
   class MainController < Volt::ModelController
+    def initialize
+      Document.ready? do 
+        `var quill = new Quill('#editor');`
+        `quill.addModule('toolbar', { container: '#toolbar' });`
+      end
+    end
   end
 end
