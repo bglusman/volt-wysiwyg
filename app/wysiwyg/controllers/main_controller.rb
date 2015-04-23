fd42a60a7bfb6e32c9dbc5a187aee25b493eced0
Brian Glusman <brian@glusman.me>
2015-04-23 16:20:06 -0400

    Working text attribute

--- app/wysiwyg/controllers/main_controller.rb
+++ app/wysiwyg/controllers/main_controller.rb
@@ -1,10 +1,20 @@
 module Wysiwyg
   class MainController < Volt::ModelController
-    def initialize
-      Document.ready? do 
-        `var quill = new Quill('#editor');`
-        `quill.addModule('toolbar', { container: '#toolbar' });`
+    attr_reader :quill, :text
+    def initialize(attributes)
+      @text = attributes.locals[:text]
+    end
+
+    def index_ready
+      puts 'index_ready'
+      `var quill = new Quill('.quill-editor');`
+      @quill = `quill`
+      `quill.addModule('toolbar', { container: '.quill-toolbar' });`
+      if `#{quill}.getText()`.blank?
+        puts 'its empty'
+        `#{quill}.insertText(0, #{text})`
       end
     end
+
   end
 end
